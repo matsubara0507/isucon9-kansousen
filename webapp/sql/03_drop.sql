877cf8a3f261b9650774714a0f16d3f09b3006aa
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2019-10-25 13:38:35 +0900

    fix to use drop

---
+++ webapp/sql/03_drop.sql
@@ -0,0 +1,16 @@
+use `isucari`;
+
+ALTER TABLE `transaction_evidences`
+    DROP COLUMN `item_name`,
+    DROP COLUMN `item_price`,
+    DROP COLUMN `item_description`,
+    DROP COLUMN `item_category_id`,
+    DROP COLUMN `item_root_category_id`;
+
+ALTER TABLE `shippings`
+    DROP COLUMN `item_name`,
+    DROP COLUMN `reserve_time`,
+    DROP COLUMN `to_address`,
+    DROP COLUMN `to_name`,
+    DROP COLUMN `from_address`,
+    DROP COLUMN `from_name`;
